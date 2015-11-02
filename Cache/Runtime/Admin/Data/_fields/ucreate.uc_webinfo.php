307a6913f9a8c9b95307678bcb4d645ef54cb7c2
chenmingbiao <mbillchan@gmail.com>
2015-11-02 10:17:36 +0800

    添加gitignore

--- Cache/Runtime/Admin/Data/_fields/ucreate.uc_webinfo.php
+++
@@ -1,1 +0,0 @@
-a:7:{i:0;s:2:"id";i:1;s:7:"content";i:2;s:4:"type";i:3;s:10:"controller";i:4;s:12:"created_time";s:3:"_pk";s:2:"id";s:5:"_type";a:5:{s:2:"id";s:7:"int(11)";s:7:"content";s:11:"varchar(80)";s:4:"type";s:11:"varchar(20)";s:10:"controller";s:11:"varchar(32)";s:12:"created_time";s:11:"varchar(20)";}}
--- Cache/Runtime/Index/Data/_fields/ucreate.uc_visitor.php
+++
@@ -1,1 +0,0 @@
-a:7:{i:0;s:2:"id";i:1;s:2:"ip";i:2;s:7:"country";i:3;s:4:"area";i:4;s:12:"created_time";s:3:"_pk";s:2:"id";s:5:"_type";a:5:{s:2:"id";s:7:"int(11)";s:2:"ip";s:11:"varchar(20)";s:7:"country";s:11:"varchar(80)";s:4:"area";s:11:"varchar(80)";s:12:"created_time";s:11:"varchar(20)";}}
